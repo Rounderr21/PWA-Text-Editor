05474d3138bc2183e85ae3e6a4ba3d7cd71eaf49
Cody Schwengler01~ <cschwengler@outlook.com>
2023-12-18 21:31:49 -0800

    created database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to add content to the database
+export const putDb = async (content) => {
+   // Create a connection to the database database and version we want to use.
+  const db = await openDB('jate', 1);
+    // Create a new transaction and specify the database and data privileges.
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+    // Use the .add() method on the store and pass in the content.
+  const request = store.put({id: 1, value: content});
+   // Get confirmation of the request.
+  const result = await request;
+  console.log('Content added to the database:', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic to get all content from the database
+export const getDb = async () => {
+    // Create a connection to the database database and version we want to use.
+  const db = await openDB('jate', 1);
+    // Create a new transaction and specify the database and data privileges.
+  const tx = db.transaction('jate', 'readonly');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+   // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+    // Get confirmation of the request.
+  const result = await request;
+  console.log('All content from the database:', result);
+  return result?.value;
+};
 
-initdb();
+initdb(); // Initialize the database when the module is imported
