1b767f074450b5e6d957b252d55bcc8d0cd09621
Cody Schwengler01~ <cschwengler@outlook.com>
2023-12-27 16:32:03 -0800

    corrected webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const workboxPlugin = require('workbox-webpack-plugin');
+const cssPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -42,7 +41,10 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new cssPlugin({
+        filename: 'src/css/style.css',
       }),
     ],
 
@@ -50,24 +52,17 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: [cssPlugin.loader, 'css-loader'],
         },
         {
           // babel loader
-          test: /\.m?js$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/transform-runtime',
-              ],
             },
           },
-        },
-      ],
-    },
+        ],
+      },
   };
 };
